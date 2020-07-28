a82d3f7d8d24c546315735a3956d2eb57a89a044
pedromesmer <pedro_mesmer@hotmail.com>
2020-07-27 22:56:53 -0300

    bug dos caracteres corrigido

--- crawlet.js
+++ crawlet.js
@@ -1,54 +1,34 @@
 const request = require('request')
 const cheerio = require('cheerio')
-const Entities = require('html-entities').AllHtmlEntities
 const Promise = require('promise')
 
-
-const entities = new Entities()
-
-//https://evangelhoespirita.wordpress.com/
-function searchGospel() {
-    request('https://kardecpedia.com/roteiro-de-estudos/887/o-evangelho-segundo-o-espiritismo', (err, res, body) => {
-        if (err) console.log('Erro')
-    
-        const $ = cheerio.load(body)
-        let capitulo
-        let subcapitulo
-    
-        const elemento = $('#menu > li').find('> a')   
-        
-        capitulos = elemento.text().replace(/\n\t\t\t\t/g, '').replace(/\t/g, '\n').replace(/\n\n\n/g, '\n').replace(/\n\n/g, '\n').trim().split('\n')
-        console.log(capitulos)
-    
-        //console.log('Conteudo: ')
-        
-    })
-}
 
 async function randomGospel() {
 
     return new Promise( (resolve, reject) => {
-        request('http://www.oconsolador.com.br/mensagens.asp?r=1',
-            {encoding: null}, (err, res, body) => {
+        // 1 ~ 243
+        const random = Math.floor(Math.random() * 243 + 1)
+
+        request(`http://oconsolador.com.br/print.asp?id=${random}`, {encoding: 'latin1'},
+        function (err, res, body) {
+        
             if (err) {
                 console.log('Erro cara: ', err)
                 reject(err)
             }
+
+            //console.log(body.toString()) // pagina
     
             const $ = cheerio.load(body, {decodeEntities: false})
-            const titulo = $('p > b span font').html().trim().replace('\n', '')
-            const texto = $('p > span font').html().trim()
-    
-            //entities.decode(componente)
+            const titulo = $('h1').html().trim()
+            const texto = $('h2').html().trim()
 
-            // \<[\w\s\W]+\> -> remove os < , > e tudo dentro deles
-            // \([\w\s\W]+\) -> remove os ( , ) e tudo dentro deles
-            const splitMsg = (titulo + '\n . ' + texto).toString().replace(/\<[\w\s\W]+\>/, ' ').replace(/\([\w\s\W]+\)/, ' ')
-
+            // \.  [\w\s\W]+/ -> tudo que vinher depois do \.
+            // \<[\w\s\W]+\> -> < , > e tudo dentro deles
+            // \([\w\s\W]+\) -> ( , ) e tudo dentro deles
+            const splitMsg = (titulo + '. ' + texto).toString().replace(/\<[\w\s\W]+\>/, ' ').replace(/\([\w\s\W]+\)/, ' ')          
             
-            
-            //console.log(splitMsg)
-
+            //console.log(splitMsg) //  string formatada
 
             resolve(
                 splitMsg
@@ -60,4 +40,3 @@ async function randomGospel() {
 randomGospel()
 
 module.exports.randomGospel = randomGospel
-module.exports.searchGospel = searchGospel
